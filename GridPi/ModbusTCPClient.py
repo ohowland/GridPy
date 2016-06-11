b606781a8a30f596b8ca9154a278d2c7cc38e489
Owen Edgerton <ohowland@gmail.com>
2016-06-10 17:33:22 -0700

    try again

--- GridPi/ModbusTCPClient.py
+++ GridPi/ModbusTCPClient.py
@@ -1,16 +1,16 @@
-from pymodbus.client.sync import ModbusTcpClient
-from pymodbus.payload import BinaryPayloadDecoder
+from pymodbus3.client.sync import ModbusTcpClient
+from pymodbus3.payload import BinaryPayloadDecoder
 from time import sleep
 
+
 class ModbusTCPClient(object):
+    """ModbusTCPClient polls a Modbus address
+    for data returns as dictionary
+    """
 
-    '''ModbusTCPClient polls a MODBUS address
-    for data returns as dictonary
-    '''
+    def __init__(self, filepath):
 
-    def __init__(self, FilePath):
-
-        '''PARAMETERS:
+        """PARAMETERS:
         self.config_path = Location of the configuration file
             for this deployment.
         self.reg = [{
@@ -20,53 +20,37 @@ class ModbusTCPClient(object):
             'type' : '32bit_float, 16bit_int',
             'value' : 'current register value'
         }]
-        '''
+        """
 
-        self.config_path = FilePath
-        self.reg = [{
-            'name' : 'kW',
-            'ip' : '192.168.253.154',
-            'address' : 42,
-            'type' : '32bit_float',
-            'value' : 0
-        }, {
-            'name' : 'kVAR',
-            'ip' : '192.168.253.154',
-            'address' : 44,
-            'type' : '32bit_float',
-            'value' : 0
-        }]
+        self.config_path = filepath
+        self.reg = dict()
 
-        self.LoadConfig(self.config_path)
-
+        self.load_config(self.config_path)
+        self.client = ModbusTcpClient(self.reg[0]['ip'])
 
     def __del__(self):
 
-        '''Teardown'''
+        """Teardown"""
 
         self.client.close()
 
+    def load_config(self, filepath):
 
-    def LoadConfig(self, FilePath):
-
-        '''Take .xml or .json file
+        """Take .xml or .json file
         and convert to list of dictonaries in the form
         of self.registers
-        '''
+        """
 
+    def init_connection(self):
 
-    def InitConnection(self):
-
-        '''Initiates the ModbusTCP connection'''
+        """Initiates the ModbusTCP connection"""
 
         try:
-            self.client = ModbusTcpClient(self.reg[0]['ip'])
             self.client.connect()
         except:
             print('Bad IP Address')
 
-
-    def read(self, register_name_list=[]):
+    def read(self):
 
         for register in self.reg:
 
@@ -74,40 +58,38 @@ class ModbusTCPClient(object):
 
             if register['type'] == '32bit_float':
                 read_data = self.client.read_holding_registers(register['address'], 2, unit=1)
-                decoded_data = BinaryPayloadDecoder.fromRegisters(list(reversed(read_data.registers)), endian='>')
+                decoded_data = BinaryPayloadDecoder.from_registers(list(reversed(read_data.registers)), endian='>')
                 register['value'] = decoded_data.decode_32bit_float()
 
             elif register['type'] == '16bit_int':
                 read_data = self.client.read_holding_registers(register['address'], 1, unit=1)
-                decoded_data = BinaryPayloadDecoder.fromRegisters((read_data.registers), endian='>')
+                decoded_data = BinaryPayloadDecoder.from_registers(read_data.registers, endian='>')
                 register['value'] = decoded_data.decode_16bit_int()
 
             else:
-                Print(register['type'], 'data type not supported')
+                print(register['type'], 'data type not supported')
 
     def write(self):
 
-        '''TODO: write holding registers'''
+        """TODO: write holding registers"""
 
     def get(self, name):
         for reg in self.reg:
             if reg['name'] == name:
-                return[reg['value']]
+                return [reg['value']]
             else:
-                return[0]
+                return [0]
 
 
 if __name__ == '__main__':
 
     ModTCP = ModbusTCPClient('C:/')
-    ModTCP.InitConnection()
+    ModTCP.init_connection()
 
     while True:
         ModTCP.read()
-        
+
         print(ModTCP.get('kW'))
         sleep(1)
         print(ModTCP.get('kVAR'))
         sleep(1)
-
-
